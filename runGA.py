a9dc6f403e6b142a86f1386d85bdadb48c74b59a
datmemerboi <shravanssd@gmail.com>
2020-05-07 20:32:08 +0530

    Optimization of obj_function

--- runGA.py
+++ runGA.py
@@ -1,30 +1,33 @@
 from GA import GeneticAlgorithm
 
-# Fn to be used by the algorithm
-def fitness_function(genome):
+def fitting_fn(genome):
 	x1, x2, x3, x4, x5, x6 = genome
+
+	if x5==x6:
+		x6=0
+	if x4==x5 or x4==x6:
+		x5 = x6 = 0
 	if x4>x5 and x4>x6:
 		x5 = x6 = 0
 	if x5>x6 and x5>x4:
 		x4 = x6 = 0
 	if x6>x4 and x6>x5:
 		x4 = x5 = 0
-	# return x1*0.9 + x2*0.5 + x3*0.1 +  (1-( x4*x5 ) )
-	return (x1 + (10-(x2/x3)) + (x4*9) + (x5*5) + (x6*1))
 
-# Size of Population used in algorithm
-population_size = 100
+	if x3==0:
+		return 0
+	else:
+		return x1 + (10-(x2/x3)) + (x4*9) + (x5*5) + (x6*1)
 
-# Number of arugment returned from fitness_function
-genome_length = 6
+population_size = 100; genome_length = 5
 
 ga = GeneticAlgorithm(
 	fitness_function, 
 	pop_size=population_size, 
 	genome_length=genome_length,
-	lb=[5, 11, 16, 0, 0, 0],
-	ub=[10, 15, 20, 2, 2, 2]
-)
+	lb=[1, 1, 0, 0, 0],
+	ub=[20, 20, 2, 2, 2]
+	)
 ga.generate_binary_population()
 
 # No. of pairs of individuals to be picked to mate
@@ -33,11 +36,10 @@ ga.number_of_pairs = 4
 # Selective pressure from interval [1.0, 2.0]
 # the lower value, the less will the fitness play role
 
-# ga.selective_pressure = 1.5
-# ga.mutation_rate = 0.1
+ga.selective_pressure = 1.4
+ga.mutation_rate = 0.1
 
-# Mention number of iterations to run algorithm
-ga.run(1000)
+ga.run(100)
 
 best_genome, best_fitness = ga.get_best_genome()
 print("Best values for x: ", (1*best_genome))
--- runPSO.py
+++ runPSO.py
@@ -1,11 +1,29 @@
 from PSO import PSO
 
-def adder(arguments):
-	# x1, x2, x3 = arguments
-	return sum(arguments)
+def fitting_fn(genome):
+	x1, x2, x3, x4, x5, x6 = genome
 
+	if x5==x6:
+		x6=0
+	if x4==x5 or x4==x6:
+		x5 = x6 = 0
+	if x4>x5 and x4>x6:
+		x5 = x6 = 0
+	if x5>x6 and x5>x4:
+		x4 = x6 = 0
+	if x6>x4 and x6>x5:
+		x4 = x5 = 0
 
-instance = PSO(func=adder, dim=3, lb=[1, 2, 3], ub=[10, 20, 30])
+	if x3==0:
+		return 0
+	else:
+		return x1 + (10-(x2/x3)) + (x4*9) + (x5*5) + (x6*1)
+
+instance = PSO(
+	func=fitness_fn, dim=3,
+	lb=[1, 1, 0, 0, 0],
+	ub=[20, 20, 2, 2, 2]
+	)
 result = instance.run(max_iter=100)
 
 print("Best values of x: ", result.gbest_x)
